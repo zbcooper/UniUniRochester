1764f832a8de26ce6b3c6e1339a9163112e92f6d
Bo Zhao <bozhao@Bos-MacBook-Pro.local>
2023-06-05 22:51:35 -0500

    add order button

--- BonanaHomePageBehavior.js
+++ BonanaHomePageBehavior.js
@@ -46,3 +46,30 @@ const imageSlider = (function () {
 })();
 
 imageSlider.init();
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('menu-link').addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        fetch('menu.html')
+        .then(response => response.text())
+        .then(data => {
+            const contentDiv = document.getElementById('content');
+            contentDiv.innerHTML = data;
+            contentDiv.id = 'menu'; // Update the ID to 'menu'
+            
+            // Jump to the newly populated 'menu' section
+            window.location.hash = 'menu';
+        });
+    });
+});
+
+
+
